84486096f5e143ba379c3077e4780fe49c111916
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-02-12 20:01:29 +0530

    Contact page added

---
+++ Script/script.js
@@ -0,0 +1,19 @@
+// Add to cart
+ debugger;
+let count = 0
+let atc = document.getElementById("product-count")
+
+let addtocart = () => {
+    debugger;
+
+    if (count < 10) {
+        count++
+        atc.innerText = count;
+    } 
+}
+
+
+// Redirect to Shop page
+
+
+
