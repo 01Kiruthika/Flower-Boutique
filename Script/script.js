76021360eade8ab972c2c18ad9722c8e5831cf1d
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-02-13 12:49:54 +0530

    Index page Modified

--- Script/script.js
+++ Script/script.js
@@ -1,19 +1,32 @@
-// Add to cart
- debugger;
-let count = 0
-let atc = document.getElementById("product-count")
+// // Add to cart
+//  debugger;
+// let count = 0
+// let atc = document.getElementById("product-count")
 
-let addtocart = () => {
-    debugger;
+// // let addtocart = () => {
+// //     debugger;
 
-    if (count < 10) {
-        count++
-        atc.innerText = count;
-    } 
-}
+// //     if (count < 10) {
+// //         count++
+// //         atc.innerText = count;
+// //     } 
+// // }
 
 
 // Redirect to Shop page
+let shoppage = () => {
+     // alert("Hello")
+    window.location.href = "http://127.0.0.1:5502/shop.html";
+}
 
 
+//Redirect to view.html
+let addtocart = () => {
+    // alert("Hello")
+    window.location.href = "http://127.0.0.1:5502/view.html"
 
+}
+let aboutpage=()=>{
+    // alert("Hello")
+    window.location.href = "http://127.0.0.1:5502/about.html"
+}
