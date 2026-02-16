78180318822b67738275e386be23dcfba685fd9f
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-02-16 10:58:23 +0530

    login page add

--- Script/script.js
+++ Script/script.js
@@ -1,16 +1,16 @@
-// // Add to cart
-//  debugger;
-// let count = 0
-// let atc = document.getElementById("product-count")
+// Add to cart
+ debugger;
+let count = 0
+let atc = document.getElementById("product-count")
 
-// // let addtocart = () => {
-// //     debugger;
+let addtocart = () => {
+    debugger;
 
-// //     if (count < 10) {
-// //         count++
-// //         atc.innerText = count;
-// //     } 
-// // }
+    if (count < 1) {
+        count++
+        atc.innerText = count;
+    } 
+}
 
 
 // Redirect to Shop page
@@ -19,13 +19,6 @@ let shoppage = () => {
     window.location.href = "http://127.0.0.1:5502/shop.html";
 }
 
-
-//Redirect to view.html
-let addtocart = () => {
-    // alert("Hello")
-    window.location.href = "http://127.0.0.1:5502/view.html"
-
-}
 let aboutpage=()=>{
     // alert("Hello")
     window.location.href = "http://127.0.0.1:5502/about.html"
